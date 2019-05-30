9cb459275f99ca2e6e50ad819e61ed6939c91f63
a-ravitz <adam.ravitz@gmail.com>
2019-05-29 21:42:46 -0400

    updated call to database

--- public/js/member.js
+++ public/js/member.js
@@ -9,12 +9,24 @@ $(document).ready(function() {
     var userId = getUrlParameter('userId')
     console.log("user ID = " + userId)
 
-
-    $.get("api/member")
-
-
-
-
-
-
+        $.get("/api/result", function(req, res){
+          
+                if(req[req.length-1].UserId === parseInt(userId)) {
+                    console.log(req[req.length-1].createdAt)
+                    console.log(req[req.length-1])
+                    
+                    $("<li>").text("Searched For On: " +req[req.length-1].createdAt).appendTo("#list")
+                    $("<li>").html("<a href='" + req[req.length-1] + "'></a>").appendTo("#list")
+                    
+                    
+                      
+                  
+                }
+                
+                
+            })
+            
 });
+
+
+
